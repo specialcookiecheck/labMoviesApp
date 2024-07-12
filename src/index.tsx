a47f2040d865a4e1d7d22cf13fc298fe74c4219e
spcialcookiecheck <vincentgroeneveld_@hotmail.com>
2024-07-12 07:33:54 -0700

    Home page components.

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { createRoot } from "react-dom/client";
+import HomePage from "./pages/homePage.tsx";
+import sample from "./stories/sampleData";
+
+const movies = [sample, sample, sample, sample, sample, sample, sample];
+
+const App = () => {
+  return <HomePage movies={movies} />;
+};
+
+const rootElement = createRoot(document.getElementById("root")!);
+rootElement.render(<App />);
