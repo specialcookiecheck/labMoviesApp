32a43235af536f6ef2a92bdf6b21b9589f31f902
spcialcookiecheck <vincentgroeneveld_@hotmail.com>
2024-07-13 05:49:03 -0700

    Refactor: Template list page; HTTP API calls centralised.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -50,3 +50,7 @@ export interface MoviePageProps {
 }
 
 export type FilterOption = "title" | "genre";
+
+export interface MovieListPageTemplateProps extends BaseMovieListProps {
+  title: string;
+}
