ec28169d1f5b768ba30caf832b651badc537a164
spcialcookiecheck <vincentgroeneveld_@hotmail.com>
2024-07-18 02:25:08 -0700

    Show movie review excerpts; Refactor Details page.

--- src/api/tmdb-api.ts
+++ src/api/tmdb-api.ts
@@ -6,26 +6,37 @@ export const getMovies = () => {
       .then(json => json.results);
   };
   
-  export const getMovie = ( id : string) => {
-    return fetch(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_TMDB_KEY}`
-    ).then(res => res.json());
-  };
-  
-  export const getGenres = () => {
-    return fetch(
-      "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
-        import.meta.env.VITE_TMDB_KEY +
-        "&language=en-US"
-    )
-      .then(res => res.json())
-      .then(json => json.genres);
-  };
-  
-   export const getMovieImages = ( id : string | number) => {
-    return fetch(
-      `https://api.themoviedb.org/3/movie/${id}/images?api_key=${import.meta.env.VITE_TMDB_KEY}`
-    )
-      .then((res) => res.json())
-      .then((json) => json.posters);
-  };
+export const getMovie = ( id : string) => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_TMDB_KEY}`
+  ).then(res => res.json());
+};
+
+export const getGenres = () => {
+  return fetch(
+    "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
+      import.meta.env.VITE_TMDB_KEY +
+      "&language=en-US"
+  )
+    .then(res => res.json())
+    .then(json => json.genres);
+};
+
+  export const getMovieImages = ( id : string | number) => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${id}/images?api_key=${import.meta.env.VITE_TMDB_KEY}`
+  )
+    .then((res) => res.json())
+    .then((json) => json.posters);
+};
+
+export const getMovieReviews = (id: string | number) => { //movie id can be string or number
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${import.meta.env.VITE_TMDB_KEY}`
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      // console.log(json.results);
+      return json.results;
+    });
+};
--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -54,3 +54,9 @@ export type FilterOption = "title" | "genre";
 export interface MovieListPageTemplateProps extends BaseMovieListProps {
   title: string;
 }
+
+export interface Review{
+    id: string;
+    content: string
+    author: string
+  }
