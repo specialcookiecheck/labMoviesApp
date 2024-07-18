c8dfc44f3ffbd34bf52cddfe57dff1df9508992d
spcialcookiecheck <vincentgroeneveld_@hotmail.com>
2024-07-18 07:16:08 -0700

    Refactor to allow for new filtering hook, and add the hook.

--- src/pages/favouriteMoviesPage.tsx
+++ src/pages/favouriteMoviesPage.tsx
@@ -1,18 +1,58 @@
 import React from "react";
 import PageTemplate from "../components/templateMovieListPage";
+import useFiltering from "../hooks/useFiltering";
+import MovieFilterUI, {
+  titleFilter,
+  genreFilter,
+} from "../components/movieFilterUI";
+
+const titleFiltering = {
+  name: "title",
+  value: "",
+  condition: titleFilter,
+};
+const genreFiltering = {
+  name: "genre",
+  value: "0",
+  condition: genreFilter,
+};
 
 const FavouriteMoviesPage: React.FC = () => {
+  const { filterValues, setFilterValues, filterFunction } = useFiltering([
+    titleFiltering,
+    genreFiltering,
+  ]);
+
+  const changeFilterValues = (type: string, value: string) => {
+    const changedFilter = { name: type, value: value };
+    const updatedFilterSet =
+      type === "title"
+        ? [changedFilter, filterValues[1]]
+        : [filterValues[0], changedFilter];
+    setFilterValues(updatedFilterSet);
+  };
+
+  const favouriteMovies = JSON.parse(
+    localStorage.getItem("favourites") || "[]"
+  );
+
+  const displayedMovies = filterFunction(favouriteMovies);
+
   const toDo = () => true;
-  // Get movies from local storage.
-  const movies = JSON.parse(localStorage.getItem("favourites") || "[]");
-  console.log(movies);
 
   return (
-    <PageTemplate
-      title="Favourite Movies"
-      movies={movies}
-      selectFavourite={toDo}
-    />
+    <>
+      <PageTemplate
+        title="Favourite Movies"
+        movies={displayedMovies}
+        selectFavourite={toDo}
+      />
+      <MovieFilterUI
+        onFilterValuesChange={changeFilterValues}
+        titleFilter={filterValues[0].value}
+        genreFilter={filterValues[1].value}
+      />
+    </>
   );
 };
 
