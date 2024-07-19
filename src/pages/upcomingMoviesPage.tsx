28448ff181bd8f5d553cee249f2f89f2f3e58dbb
spcialcookiecheck <vincentgroeneveld_@hotmail.com>
2024-07-19 04:37:12 -0700

    Fix upcoming movies page.

--- src/pages/upcomingMoviesPage.tsx
+++ src/pages/upcomingMoviesPage.tsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import { getUpcomingMovies } from "../api/tmdb-api";
+import AddToFavouritesIcon from "../components/cardIcons/addToFavourites";
 
 const UpcomingMoviesPage: React.FC = () => {
   const [movies, setMovies] = useState<BaseMovieProps[]>([]);
-  const toDo = () => true;
-  // Get movies from local storage.
   console.log(movies);
 
   useEffect(() => {
@@ -19,7 +18,9 @@ const UpcomingMoviesPage: React.FC = () => {
     <PageTemplate
       title="Upcoming Movies"
       movies={movies}
-      selectFavourite={toDo}
+      action={(movie: BaseMovieProps) => {
+        return <AddToFavouritesIcon {...movie} />;
+      }}
     />
   );
 };
